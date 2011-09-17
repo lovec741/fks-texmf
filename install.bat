f9dba45376fdda8784e21151c43450ae5200979c
Michal Koutny <xm.koutny@gmail.com>
2011-09-17 22:18:40 +0200

    Instalační skripty

---
+++ install.bat
@@ -0,0 +1,4 @@
+-- TODO
+
+-- update filename database
+initexmf --update-fndb
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEXMF="$HOME/texmf/" # change this to your user texmf dir
+LATEX="tex/latex/fykosx"
+
+if [ -d "$TEXMF$LATEX" -a "$1" != "-f" ] ; then
+	echo "Macros 'fykosx' already installed. Use -f to reinstall."
+fi
+
+# preapre directories
+rm -rf "$TEXMF$LATEX"
+
+# copy files
+cp -r "./$LATEX" "$TEXMF$LATEX"
+
+# update kpathsearch
+mktexlsr
