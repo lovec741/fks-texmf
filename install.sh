309d13397b47752d93f4c18aa5042f15d5acd891
Michal Koutny <xm.koutny@gmail.com>
2011-09-22 02:56:19 +0200

    Makra pro metapost

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 TEXMF="$HOME/texmf/" # change this to your user texmf dir
 LATEX="tex/latex/fykosx" # path to store fykosx macros
+MPOST="metapost/fykos" # path to metapost macros
 
 if [ -d "$TEXMF$LATEX" -a "$1" != "-f" ] ; then
 	echo "Macros 'fykosx' already installed. Use -f to reinstall."
@@ -10,9 +11,11 @@ fi
 
 # preapre directories
 rm -rf "$TEXMF$LATEX"
+rm -rf "$TEXMF$MPOST"
 
 # copy files
 cp -r "./$LATEX" "$TEXMF$LATEX"
+cp -r "./$MPOST" "$TEXMF$LATEX"
 
 # update kpathsearch
 mktexlsr
